9f5d6a7d8c0fc7afdaae685ec8311f3f0a5d7e74
robertoconoscenti-boop <robertoconoscenti33@gmail.com>
2026-08-05 18:40:40 +0200

    Refresh service worker cache for mobile PWA release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='clearchoice-v1';
+const CACHE='clearchoice-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon.svg','./lib/decision-engine.js','./lib/storage.js','./lib/export.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
